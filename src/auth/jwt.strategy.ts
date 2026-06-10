960e9d0d940ca42723eec6336f2a0dec148f665a
Shiva Pal <spal13138@gmail.com>
2026-06-10 17:13:11 +0530

    configuration for deployment

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'my-secret-key',
+      secretOrKey: 'my-super-secret-key',
     });
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe(),
   );
 
-  await app.listen(3000);
-}bootstrap();
+  await app.listen(
+    process.env.PORT || 3000,
+  );
+}
+
+bootstrap();
